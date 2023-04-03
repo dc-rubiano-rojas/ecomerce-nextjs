0d66e48c9bbb912a5fa8c769996dcd4c7cfe6464
dc-rubiano-rojas <dcrubiano02@gmail.com>
2023-04-03 16:33:53 -0500

    :fire: schemes no using

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,8 +1,5 @@
 import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
 import product from './product'
 import banner from './banner'
 
-export const schemaTypes = [post, author, category, blockContent, product, banner]
+export const schemaTypes = [blockContent, product, banner]
